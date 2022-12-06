a6f2897ed56929a660c2e25e5ff6892d5de081dd
ahmedabzk <ahmed.abzk@gmail.com>
2022-12-06 12:21:05 +0300

    transfer api with custom validator

--- api/transfer.go
+++ api/transfer.go
@@ -13,7 +13,7 @@ type createTransferRequest struct {
 	FromAccountID int64  `json:"from_account_id" binding:"required"`
 	ToAccountID   int64  `json:"to_account_id" binding:"required"`
 	Amount        int64  `json:"amount" binding:"required"`
-	Currency      string `json:"currency" binding:"required, currency"`
+	Currency      string `json:"currency" binding:"required,currency"`
 }
 
 func (server *Server) createTransfer(ctx *gin.Context) {
@@ -24,27 +24,27 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 		return
 	}
 
-	if !server.validAccount(ctx, req.FromAccountID, req.Currency){
-		return 
+	if !server.validAccount(ctx, req.FromAccountID, req.Currency) {
+		return
 	}
 
-	if !server.validAccount(ctx, req.ToAccountID, req.Currency){
-		return 
+	if !server.validAccount(ctx, req.ToAccountID, req.Currency) {
+		return
 	}
 
-	arg := db.CreateTransferParams{
+	arg := db.TransferTxParams{
 		FromAccountID: req.FromAccountID,
 		ToAccountID:   req.ToAccountID,
 		Amount:        req.Amount,
 	}
 
-	transfer, err := server.store.CreateTransfer(ctx, arg)
+	result, err := server.store.TransferTx(ctx, arg)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, ErrorResponds(err))
 		return
 	}
 
-	ctx.JSON(http.StatusOK, transfer)
+	ctx.JSON(http.StatusOK, result)
 }
 
 func (server *Server) validAccount(ctx *gin.Context, accountID int64, currency string) bool {
@@ -59,7 +59,7 @@ func (server *Server) validAccount(ctx *gin.Context, accountID int64, currency s
 		return false
 	}
 
-	if account.Currency != currency{
+	if account.Currency != currency {
 		err := fmt.Errorf("account [%d] currency mismatch: %s vs %s", accountID, account.Currency, currency)
 		ctx.JSON(http.StatusBadRequest, ErrorResponds(err))
 		return false
